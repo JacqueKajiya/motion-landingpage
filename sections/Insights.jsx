32caa63cf1e0ea6ef82de4e702c46aa63576cf54
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-11-09 12:40:00 -0300

    feat: added World section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+
+import { staggerContainer, fadeIn } from '../utils/motion';
+import { TitleText, TypingText } from '../components';
+
 const Insights = () => (
   <section>
     Insights section
